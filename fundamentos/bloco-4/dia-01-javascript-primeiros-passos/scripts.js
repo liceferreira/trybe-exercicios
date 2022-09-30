e506c9cf609107489b4d3f6897e5f248f73170f1
Alice <alicesanfe96@gmail.com>
2022-09-30 09:42:47 -0300

    Exercício para fixar 4 concluído

--- fundamentos/bloco-4/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4/dia-01-javascript-primeiros-passos/scripts.js
@@ -40,6 +40,25 @@ const nota = 83;
     // else {
     //     console.log("Você foi reprovado(a)")
     
+//  Exercício para fixar 4 
 
+const currentHour = 9;
+let message = "";
 
+    if (currentHour >= 22) {
+        message = "Não deveríamos comer nada agora, é hora de dormir";
+    }
+    else if (currentHour >= 18 && currentHour < 22) {
+        message = "Rango da noite, vamos jantar :D";
+    }
+    else if (currentHour >= 14 && currentHour < 18) {
+        message = "Vamos fazer um bolo para o café da tarde?";
+    }
+    else if (currentHour >= 11 && currentHour <= 14) {
+        message = "Hora do almoço!!!";
+    }
+    else if (currentHour >= 4 && currentHour <= 11) {
+        message = "Hmmmm, cheiro de café recém passado";
+    }
 
+    console.log(message);
