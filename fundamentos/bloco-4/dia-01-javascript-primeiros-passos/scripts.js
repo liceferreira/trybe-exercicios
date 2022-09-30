31af784dd4284f10cb19570e7c5593f776ff9cb1
Alice <alicesanfe96@gmail.com>
2022-09-30 09:09:03 -0300

    Exercícios para fixar 1 e 2 concluídos

---
+++ fundamentos/bloco-4/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,29 @@
+// Exercício para fixar
+
+const myName = "Alice";
+const birthCity = "Curitiba";
+let birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// const birthCity = "Poços de Caldas";
+// console.log(birthCity);
+ // No caso, receberíamos uma mensagem de erro pois a constante foi alterada, o que não é permitido.
+
+
+// Exercício para fixar 2
+
+const base = 5;
+const height = 8;
+const area = (base * height);
+const perimeter = 5 + 5 + 8 + 8;
+//const perimeter = (base + height) * 2;
+
+console.log(area);
+console.log(perimeter);
+
+// Exercício para fixar 3
+
+
