c6853ab812e29b752dca8877fa4f9de902e84aee
Alice <alicesanfe96@gmail.com>
2022-10-03 12:13:28 -0300

    Exercício 5 concluído

---
+++ fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/exercicio1.js
@@ -0,0 +1,6 @@
+// Exercício 1
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ 
+for(let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    console.log(indexNumbers);
+}
