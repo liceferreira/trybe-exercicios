a65add770743908148c006d0df15ad78ac900371
Alice <alicesanfe96@gmail.com>
2022-10-03 11:40:01 -0300

    Exercício para fixar FOR concluído

--- fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/scripts.js
@@ -1,6 +1,21 @@
-// Exercício para fixar 1
+// Exercício para fixar 1, 2 e 3 // Arrays
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
+let indexOfPortfolio = menu.indexOf('Porfifólio')
+
+menu.push('Contato');
+
 
 console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
+
+// Exercícios para fixar // For
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+    for (let i = 0; i < groceryList.length; i++) {
+        const elements = groceryList[i];
+        console.log(elements)
+    };
