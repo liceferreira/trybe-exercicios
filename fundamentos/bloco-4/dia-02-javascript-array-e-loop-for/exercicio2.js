85011c79aa36387d7cff1a30f3871af147ab80af
Alice <alicesanfe96@gmail.com>
2022-10-03 11:54:23 -0300

    Separando os exercícios em arquivos

---
+++ fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/exercicio2.js
@@ -0,0 +1,8 @@
+// Exercício 2
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let result = 0;
+
+for (resultNumber = 0; resultNumber <numbers.length; resultNumber += 1) {
+    result += numbers[resultNumber];
+};
--- fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4/dia-02-javascript-array-e-loop-for/scripts.js
@@ -27,3 +27,30 @@ let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 for (let i of names) {
     console.log(i);
 };
+
+// Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ 
+for(let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    console.log(indexNumbers);
+}
+// Exercício 2
+
+let result = 0;
+
+for (resultNumber = 0; resultNumber <numbers.length; resultNumber += 1) {
+    result += numbers[resultNumber];
+};
+
+console.log(result);
+//Exercício 3
+
+let sum = 0;
+
+for (sumNumbers = 0; sumNumbers < numbers.length; sumNumbers += 1);
+    sum += numbers[sumNumbers];
+
+let average = sum / numbers.length;
+
+console.log(average);
