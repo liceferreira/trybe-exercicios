2c2435ab594c4d274efa0eecaa102754db3270df
Alice <alicesanfe96@gmail.com>
2022-10-18 15:20:09 -0300

    Exercício 1-2 concluído

---
+++ fundamentos/bloco-07/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/Exercício 1/script4.js
@@ -0,0 +1,14 @@
+// Retire o console.log que está fora do if/else
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
