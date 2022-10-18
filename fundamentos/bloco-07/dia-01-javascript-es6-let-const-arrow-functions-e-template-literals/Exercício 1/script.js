5e164ee77078e7b59d20ca2c9a9ce5854a6fe1e5
Alice <alicesanfe96@gmail.com>
2022-10-18 15:05:18 -0300

    Exercício 1 parte 4 concluído

--- fundamentos/bloco-07/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/Exercício 1/script.js
+++ fundamentos/bloco-07/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/Exercício 1/script.js
@@ -1,6 +1,6 @@
 // Exercício 1
 // Modifique a estrutura da função para que ela seja uma arrow function;
-const testingScope(escopo) => {
+function testingScope(escopo) {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
@@ -9,7 +9,8 @@ const testingScope(escopo) => {
     var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); 
 }
 
 testingScope(true);
+
